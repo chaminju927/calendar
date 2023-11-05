f5c9cbd86c9376f03678947c99f57fb988b9d327
chaminju927 <minjoo92718@gmail.com>
2023-11-05 22:01:23 +0900

    .

--- src/component/MonthComponent.jsx
+++ src/component/MonthComponent.jsx
@@ -11,34 +11,22 @@ function MonthComponent() {
   const [currentMonth, setCurrentMonth] = useState(now);
 
   const prevMonth = () => {
-    setCurrentMonth(currentMonth.clone().subtract(1, "month"));
+    const prev = currentMonth.clone().subtract(1, "month");
+    setCurrentMonth(prev);
+    // const minMonth = moment().subtract(5, "months").format("YYYY.MM");
+    // if (currentMonth.isBefore(minMonth, "month")) {
+    //   return;
+    // }
   };
 
   const nextMonth = () => {
-    setCurrentMonth(currentMonth.clone().add(1, "month"));
+    const next = currentMonth.clone().add(1, "month");
+    setCurrentMonth(next);
+    // const maxMonth = moment().add(5, "months").format("YYYY.MM");
+    // if (currentMonth.isAfter(maxMonth, "month")) {
+    //   return;
+    // }
   };
-
-  //   const prevMonth = () => {
-  //     const currentMonth = moment(month, "YYYY.MM");
-  //     var prev = currentMonth.clone().subtract(1, "months").format("YYYY.MM");
-  //     setMonth(prev);
-  //     setClicked(clicked - 1);
-  //     // const minMonth = moment().subtract(5, "months").format("YYYY.MM");
-  //     // if (currentMonth.isBefore(minMonth, "month")) {
-  //     //   return;
-  //     // }
-  //   };
-
-  //   const nextMonth = () => {
-  //     const currentMonth = moment(month, "YYYY.MM");
-  //     const next = currentMonth.clone().add(1, "months").format("YYYY.MM");
-  //     setMonth(next);
-  //     setClicked(clicked + 1);
-  //     // const maxMonth = moment().add(5, "months").format("YYYY.MM");
-  //     // if (currentMonth.isAfter(maxMonth, "month")) {
-  //     //   return;
-  //     // }
-  //   };
 
   return (
     <div>
